ece3949e3c6ad563883c91a2794fd03f54e4436d
Er.Krishna Kumar <135963785+Krishnarov@users.noreply.github.com>
2025-01-03 00:31:59 +0530

    set haders

--- src/components/ConstentApi.jsx
+++ src/components/ConstentApi.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 function ConstentApi() {
-  return ("http://localhost:4000")
-  // return ("https://jobportalbackend-fu17.onrender.com")
+  // return ("http://localhost:4000")
+  return ("https://jobportalbackend-fu17.onrender.com")
 }
 
 export default ConstentApi
